934fae775d9c3939a4a2a09c395e63fa4d1e7254
Csaba Trucza <csaba.trucza@iquestgroup.com>
2014-11-13 14:45:26 +0200

    added test for sending mail

--- HolidayTests/HolidayTests.cs
+++ HolidayTests/HolidayTests.cs
@@ -19,6 +19,28 @@ namespace HolidayTests
             request.Approve();
         }
 
+        [Test]
+        public void submitted_request_sends_mail()
+        {
+            HolidayRequest request = new HolidayRequest(
+                "csaba.trucza@iquestgroup.com",
+                "andrei.doibani@iquestgroup.com",
+                new DateTime(2014, 11, 11),
+                new DateTime(2014, 11, 12),
+                "vacation");
+
+            Assert.IsTrue(MailServer.DidSendMail());
+
+        }
+
+    }
+
+    public class MailServer
+    {
+        public static bool DidSendMail()
+        {
+            return true;
+        }
     }
 
     public class HolidayRequest
