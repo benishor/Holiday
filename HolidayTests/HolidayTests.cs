7b189b44ddc9beb6c542a3425cd97db95d399345
Csaba Trucza <csaba.trucza@iquestgroup.com>
2014-11-13 14:38:19 +0200

    added first (empty) test

---
+++ HolidayTests/HolidayTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace HolidayTests
+{
+    [TestFixture]
+    public class HolidayTests
+    {
+        [Test]
+        public void usage()
+        {
+            
+        }
+
+    }
+}
