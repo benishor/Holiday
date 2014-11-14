453bc064d343b532537f002b290d50c30581d518
Csaba Trucza <csaba.trucza@iquestgroup.com>
2014-11-14 14:24:01 +0200

    cleanup

--- Holiday/Message.cs
+++ Holiday/Message.cs
@@ -11,29 +11,29 @@ namespace Holiday
 
         public static Message SubmissionMessage(Employee employee, Employee manager, DateTime start, DateTime end)
         {
-            const string SubmissionMessageSubject = "Cerere de concediu";
-            const string SubmissionMessageBody = "Subsemnatul {0}, angajat iQuest va rog a-mi aproba cererea de concediu de odihna pe perioada {1} - {2}.";
+            const string submissionMessageSubject = "Cerere de concediu";
+            const string submissionMessageBody = "Subsemnatul {0}, angajat iQuest va rog a-mi aproba cererea de concediu de odihna pe perioada {1} - {2}.";
 
             return new Message
             {
                 from = employee.Email,
                 to = manager.Email,
-                subject = SubmissionMessageSubject,
-                body = string.Format(SubmissionMessageBody, employee.Name, start.ToShortDateString(), end.ToShortDateString())
+                subject = submissionMessageSubject,
+                body = string.Format(submissionMessageBody, employee.Name, start.ToShortDateString(), end.ToShortDateString())
             };
         }
 
 
         public static Message ApprovalMessage(Employee employee, Employee manager, DateTime start, DateTime end)
         {
-            const string ApprovalMessageSubject = "Cerere de concediu aprobata";
-            const string ApprovalMessageBody = "Subsemnatul {0} aprob cererea de concediu de odihna pe perioada {1} - {2} pentru {3}.";
+            const string approvalMessageSubject = "Cerere de concediu aprobata";
+            const string approvalMessageBody = "Subsemnatul {0} aprob cererea de concediu de odihna pe perioada {1} - {2} pentru {3}.";
             return new Message
             {
                 from = manager.Email,
                 to = Employee.HR().Email,
-                subject = ApprovalMessageSubject,
-                body = string.Format(ApprovalMessageBody, manager.Name, employee.Name, start.ToShortDateString(), end.ToShortDateString())
+                subject = approvalMessageSubject,
+                body = string.Format(approvalMessageBody, manager.Name, employee.Name, start.ToShortDateString(), end.ToShortDateString())
             };
         }
 
