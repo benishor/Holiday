2316dfc981cf14d9a9ecb1f4b730406b18d3215b
Csaba Trucza <csaba.trucza@iquestgroup.com>
2014-11-14 11:17:15 +0200

    refactored message creation

--- Holiday/HolidayRequest.cs
+++ Holiday/HolidayRequest.cs
@@ -9,6 +9,10 @@ namespace Holiday
         private readonly DateTime start;
         private readonly DateTime end;
 
+        const string submitMessageTemplate = "Subsemnatul {0}, angajat iQuest va rog a-mi aproba cererea de concediu de odihna pe perioada {1} - {2}.";
+        const string approveMessageTemplate = "Subsemnatul {0} aprob cererea de concediu de odihna pe perioada {1} - {2} pentru {3}.";
+
+
         public HolidayRequest(string employee, string manager, DateTime start, DateTime end, string type)
         {
             this.employee = employee;
@@ -20,27 +24,33 @@ namespace Holiday
 
         private void Submit()
         {
-            var body =
-                string.Format(
-                    "Subsemnatul {0}, angajat iQuest va rog a-mi aproba cererea de concediu de odihna pe perioada {1} - {2}.",
-                    employee, start.ToShortDateString(), end.ToShortDateString());
-            SendMessage(employee, manager, "New holiday request", body);
+            var message = CreateSubmissionMessage();
+            message.Send();
         }
 
         public void Approve()
         {
-            var body = 
-                string.Format(
-                    "Subsemnatul {0} aprob cererea de concediu de odihna pe perioada {1} - {2} pentru {3}.",
-                    manager, start.ToShortDateString(), end.ToShortDateString(), employee);
-
-            SendMessage(manager, "hr", "Holiday request approved", body);
+            var message = CreateApprovalMessage();
+            message.Send();
         }
 
-        private static void SendMessage(string from, string to, string subject, string body)
+        private Message CreateSubmissionMessage()
+        {
+            var body = string.Format(submitMessageTemplate, employee, start.ToShortDateString(), end.ToShortDateString());
+            return CreateMessage(employee, manager, "New holiday request", body);
+        }
+
+        private Message CreateApprovalMessage()
+        {
+            var body = string.Format(approveMessageTemplate, manager, start.ToShortDateString(), end.ToShortDateString(),
+                employee);
+            return CreateMessage(manager, "hr", "Holiday request approved", body);
+        }
+
+        private static Message CreateMessage(string from, string to, string subject, string body)
         {
             Message message = new Message {From = from, To = to, Subject = subject, Body = body};
-            message.Send();
+            return message;
         }
     }
 }
