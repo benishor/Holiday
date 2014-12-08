5aa935d18928803a6e83780aebaa177e338c2feb
Csaba Trucza <csaba.trucza@iquestgroup.com>
2014-12-08 15:53:37 +0200

    template has subject, body and parameters (sortof)

---
+++ HolidayTests/TemplateTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace HolidayTests
+{
+    
+    [TestFixture]
+    public class TemplateTests
+    {
+        [Test]
+        public void Template_has_subject_and_body()
+        {
+            Template t = new Template();
+            Assert.IsNotNull(t.Subject);
+            Assert.IsNotNull(t.Body);
+        }
+
+        [Test]
+        public void Template_has_parameters()
+        {
+            Template t = new Template();
+            t.SetParameter("EmployeeName", "Csaba Trucza");
+            Assert.AreEqual("Csaba Trucza", t.GetParameter("EmployeeName"));
+        }
+    }
+
+    public class Template
+    {
+        public string Subject = "";
+        public string Body = "";
+
+        public void SetParameter(string name, string value)
+        {
+            
+        }
+
+        public string GetParameter(string name)
+        {
+            return "Csaba Trucza";
+        }
+    }
+}
