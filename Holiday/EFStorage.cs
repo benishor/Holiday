ed4ab8b6a059ef7540d65585d392cd290b6e1195
Csaba Trucza <csaba.trucza@iquestgroup.com>
2015-02-07 11:26:06 +0200

    created DbContext for the requests and employees

---
+++ Holiday/EFStorage.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace Holiday
+{
+    public class EFStorage: DbContext
+    {
+        public DbSet<HolidayRequest> HolidayRequests { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
